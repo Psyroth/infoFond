f2f5e223f777a54ae440f27ad48775fbd05f9d85
Philippe Veranneman <pveranne@ulb.ac.be>
2014-05-13 15:52:17 +0200

    lol

---
+++ projet/build/CMakeFiles/proj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/proj.dir/minisat/Solver.cpp.o"
+  "CMakeFiles/proj.dir/main.cpp.o"
+  "proj.pdb"
+  "proj"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
